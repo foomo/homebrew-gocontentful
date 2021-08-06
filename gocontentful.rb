faff696aeea51adcf99a561a2372d0be1482dc77
goreleaserbot <goreleaser@carlosbecker.com>
2021-08-06 14:50:52 +0200

    Brew formula update for gocontentful version v0.9.9

--- gocontentful.rb
+++ gocontentful.rb
@@ -5,20 +5,20 @@
 class Gocontentful < Formula
   desc "An Contentful Entry-Reference Mapper for Go"
   homepage "https://github.com/foomo/gocontentful"
-  version "0.9.8"
+  version "0.9.9"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/foomo/gocontentful/releases/download/v0.9.8/gocontentful_0.9.8_darwin_amd64.tar.gz"
-      sha256 "819fb950493f1bcbb873956fe24725dff1b8201be5318fd8d1a003ea080a6705"
+      url "https://github.com/foomo/gocontentful/releases/download/v0.9.9/gocontentful_0.9.9_darwin_amd64.tar.gz"
+      sha256 "5f4392a6dd65d0c9a238ada88a6ce73833a33e5200e9213ca03e5f5b4cad2c1c"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/foomo/gocontentful/releases/download/v0.9.8/gocontentful_0.9.8_linux_amd64.tar.gz"
-      sha256 "82e767574eeb47bb712453afa638b399e381e64e97f8040979abe46745532a70"
+      url "https://github.com/foomo/gocontentful/releases/download/v0.9.9/gocontentful_0.9.9_linux_amd64.tar.gz"
+      sha256 "3f0ac068fd484d8a58c0d46a99547b0b4e63cf2d545078865bd6cf248136e032"
     end
   end
 
